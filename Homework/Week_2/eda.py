68e192e9af95c19342876e5061e55233577a20dd
lioraR <liora.rosenberg@planet.nl>
2018-11-08 13:02:16 +0100

    begin

---
+++ Homework/Week_2/eda.py
@@ -0,0 +1,33 @@
+import pandas
+import csv
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+# find inconsistencies
+# clean rows with many missing columns
+# clean rows with missing values
+
+def check_empty(list)
+
+
+
+# preprocess all usable rows
+# Country”, “Region”, “Pop. Density (per sq. mi.)”, “Infant mortality (per 1000 births)” and “GDP ($ per capita) dollars
+
+# load the data into a pandas DataFrame
+# print DataFrame
+# analyze Central Tendency and the Five Number Summary
+
+# Central Tendency: the mean, median and mode.
+# calculate the standard deviation, mean, median and mode of the GDP
+# plot a histogram
+
+# Five number summary:
+# calculate Minimum, First Quartile, Median, Third Quartile and Maximum of Infant Mortality
+# create a box plot
+
+# cleaned, preprocessed and analyzed data to a .json
+
+if __name__ == "__main__":
