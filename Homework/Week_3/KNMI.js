12f4ea4ae1d2e4270b427763e35b9ba17897bf5b
lioraR <liora.rosenberg@planet.nl>
2018-11-22 17:15:08 +0100

    ik gebruik mijn waldcard

--- Homework/Week_3/KNMI.js
+++ Homework/Week_3/KNMI.js
@@ -1,5 +1,15 @@
-function createTransform(domain, range){
+// naam: Liora Rosenberg
+// Student number: 11036435
+// this file creates a website that draws a graph of the minimum and maximum temperature
 
+// global variables that define the size of graph
+var GRAPH_TOP = 0;
+var GRAPH_BOTTOM = 480;
+var GRAPH_LEFT = 50;
+var GRAPH_RIGHT = 600;
+
+// transform data to coordinates of canvas-element
+function createTransform(domain, range) {
     var domain_min = domain[0]
     var domain_max = domain[1]
     var range_min = range[0]
@@ -10,177 +20,143 @@ function createTransform(domain, range){
     var beta = range_max - alpha * domain_max
 
     // returns the function for the linear transformation (y= a * x + b)
-    return function(x){
-      return alpha * x + beta;
+    return function(x) {
+        return alpha * x + beta;
     }
 }
 
-var fileName = "x.json";
+// distracted month and year of data
+function year(datums) {
+    listYear = []
+    listMaand = []
+    for (var i = 0; i < datums.length; i++) {
+        var date = datums[i]
+        var maand = date.substring(4, 6)
+        listMaand.push(maand)
+        var year = date.substring(0, 4)
+        listYear.push(year)
+    }
+    return [listMaand, listYear]
+}
+
+// draw graph
+function graph(data, datums, listMaand, listYear) {
+    var canvas = document.getElementById("Canvas");
+    var ctx = canvas.getContext("2d");
+
+    // temperature will not reach -9.9 C
+    var tempMaand = "-99";
+
+    // create data and lines on x-as
+    for (var x = 0, j = 0; x < 12 && j < 366; x++, j++) {
+        if (listMaand[j] != tempMaand) {
+            tempMaand = listMaand[j];
+            ctx.moveTo(GRAPH_LEFT + GRAPH_RIGHT / 12 * x, GRAPH_TOP)
+            ctx.lineTo(GRAPH_LEFT + GRAPH_RIGHT / 12 * x, GRAPH_BOTTOM)
+            ctx.fillText((listMaand[j] + "-" + listYear[j]),
+                (GRAPH_LEFT + GRAPH_RIGHT / 12 * x), GRAPH_BOTTOM)
+            ctx.strokeStyle = "#BBB";
+            ctx.stroke();
+        } else {
+            x--;
+        }
+    }
+
+    // reset line color
+    ctx.strokeStyle = "#000"
+
+    minTempList = [];
+    maxTempList = [];
+    yearList = [];
+
+    // distract min and max temperature from data
+    datums.forEach(function(test) {
+        minTempList.push(data[test]["   minTemp"])
+        maxTempList.push(data[test]["   maxTemp"])
+
+    });
+
+    // take minimum and maximum temperature
+    var min = Math.min.apply(null, minTempList)
+    var max = Math.max.apply(null, maxTempList)
+    var range = max - min;
+
+    // create data and lines on y-as
+    for (var x = 0; x < 10; x++) {
+        ctx.moveTo(GRAPH_LEFT, GRAPH_TOP + x * (GRAPH_BOTTOM - GRAPH_TOP) / 10)
+        ctx.lineTo(GRAPH_RIGHT, GRAPH_TOP + x * (GRAPH_BOTTOM - GRAPH_TOP) / 10)
+        ctx.fillText(Math.round(max - x * range / 10),
+            (GRAPH_LEFT - 15), GRAPH_TOP + x * (GRAPH_BOTTOM - GRAPH_TOP) / 10)
+        ctx.strokeStyle = "#BBB";
+        ctx.stroke();
+    }
+
+    // reset line color
+    ctx.strokeStyle = "#000"
+
+    domain = [min, max]
+    range = [GRAPH_BOTTOM, GRAPH_TOP]
+    yFunction = createTransform(domain, range)
+
+    // there are 366 days in a year
+    domain = [0, 366]
+    range = [GRAPH_LEFT, GRAPH_RIGHT]
+    xFunction = createTransform(domain, range)
+
+    // make x and y-axis labels
+    var canvas = document.getElementById("Canvas");
+    var context = canvas.getContext("2d");
+    context.font = "15px sans-serif";
+    context.fillText('Date', (GRAPH_RIGHT - GRAPH_LEFT) / 2 + GRAPH_LEFT,
+        GRAPH_BOTTOM + 30);
+    context.rotate(-90 * Math.PI / 180);
+    context.fillText('Temperature (Celsius)', -(GRAPH_BOTTOM - GRAPH_TOP) / 2 - 50,
+        GRAPH_LEFT - 30);
+    context.rotate(90 * Math.PI / 180);
+
+    // draw graph
+    context.beginPath();
+    context.moveTo(GRAPH_LEFT, GRAPH_TOP);
+    context.lineTo(GRAPH_LEFT, GRAPH_BOTTOM);
+    context.moveTo(GRAPH_LEFT, GRAPH_BOTTOM);
+    context.lineTo(GRAPH_RIGHT, GRAPH_BOTTOM);
+    context.stroke();
+
+    // draw line of minimum temperature
+    context.beginPath();
+    context.moveTo(GRAPH_LEFT, minTempList[0])
+    for (var i = 0; i < minTempList.length; i++) {
+        context.lineTo(xFunction(i), yFunction(minTempList[i]))
+    }
+    context.strokeStyle = "#0000FF";
+    context.stroke();
+
+    // draw line of maximum temperature
+    context.beginPath();
+    context.moveTo(GRAPH_LEFT, minTempList[0])
+    for (var i = 0; i < minTempList.length; i++) {
+        context.lineTo(xFunction(i), yFunction(maxTempList[i]))
+    }
+    context.strokeStyle = "#8B0000";
+    context.stroke();
+
+}
+
+// load and spite jason file
+var fileName = "KNMI.json";
 var txtFile = new XMLHttpRequest();
 txtFile.onreadystatechange = function() {
     if (txtFile.readyState === 4 && txtFile.status == 200) {
-        console.log(JSON.parse(txtFile.responseText));
 
-        data = JSON.parse(txtFile.responseText);
+        var data = JSON.parse(txtFile.responseText);
+        var datums = Object.keys(data);
 
-        datums = Object.keys(data);
+        var maandYear = year(datums)
+        var listMaand = maandYear[0]
+        var listYear = maandYear[1]
 
-        listYearMaand = []
-
-        for (var i = 0; i < datums.length; i++){
-          date = datums[i]
-          yearMaand = parseInt(date.substring(0,7))
-          listYearMaand.push(yearMaand)
-          //maand = date.substring(5,7)
-        }
-
-        console.log(datums);
-
-        // uit data moet ik min_temp and max_temp pakken
-        // in de lijst zet ik de min en maximum lijsten
-        minTempList = [];
-        maxTempList = [];
-        yearList = [];
-
-        datums.forEach(function(test){
-          console.log(test);
-          console.log(data[test]["   Min_temp"]);
-          minTempList.push(data[test]["   Min_temp"])
-
-          console.log(data[test]["   Max_temp"]);
-          maxTempList.push(data[test]["   Max_temp"])
-
-        });
-
-        // hierin werken om het domein en range aan te passen
-        // de min van de minimum temp pakken en de max van de maximumtemp pakken
-        min = Math.min.apply(null, minTempList)
-        max = Math.max.apply(null, maxTempList)
-        console.log(min, max);
-
-        domain = [min, max]
-        range = [480, 0]
-
-        yFunction = createTransform(domain, range)
-
-        // ik moet het aantal jaar om de x-as zetten
-        //var minY = Math.min.apply(null, datums)
-        //var maxY = Math.max.apply(null, datums)
-        var minYM = Math.min.apply(null, listYearMaand)
-        var maxYM = Math.max.apply(null, listYearMaand)
-
-        domain = [minYM, maxYM]
-        console.log(domain);
-        range = [0, 480]
-        xFunction = createTransform(domain, range)
-
-        var canvas = document.getElementById( "Canvas" );
-        var context = canvas.getContext( "2d" );
-        context.fillText('Date', 250,500);
-
-        // groter maken canvas
-        // grafiek maken
-        var GRAPH_TOP = 0;
-        var GRAPH_BOTTOM = 480;
-        var GRAPH_LEFT = 0;
-        var GRAPH_RIGHT = 480;
-
-        //context.clearRect( 0, 0, 500, 400 );
-
-        context.beginPath();
-        context.moveTo( GRAPH_LEFT, GRAPH_TOP );
-        context.lineTo( GRAPH_LEFT, GRAPH_BOTTOM );
-        context.moveTo( GRAPH_LEFT, GRAPH_BOTTOM );
-        context.lineTo( GRAPH_BOTTOM, GRAPH_RIGHT );
-        //context.closePath();
-        context.stroke();
-
-        //var x = 0;
-        context.beginPath();
-        context.moveTo(listYearMaand[0], minTempList[0])
-        for (var i = 0; i < minTempList.length; i++){
-          // x = x+10
-          //x = yearList
-          console.log(listYearMaand[i])
-          console.log(minTempList[i])
-          context.lineTo(i, yFunction(minTempList[i]))
-        }
-        context.strokeStyle = "#0000FF";
-        context.stroke();
-
-        context.beginPath();
-        context.moveTo(listYearMaand[0], minTempList[0])
-        for (var i = 0; i < minTempList.length; i++){
-          // x = x+10
-          //x = yearList
-          console.log(listYearMaand[i])
-          console.log(minTempList[i])
-          context.lineTo(i, yFunction(maxTempList[i]))
-        }
-        context.strokeStyle = "#8B0000";
-        context.stroke();
-
-        // grijze tussenlijnen maken
-        context.beginPath();
-        context.moveTo( GRAPH_LEFT, ( GRAPH_BOTTOM ) / 2 );
-        context.lineTo( GRAPH_RIGHT, ( GRAPH_BOTTOM ) / 2 );
-        context.globalAlpha = 0.3
-        context.strokeStyle = "#BBB";
-        context.stroke();
-
-        context.beginPath();
-        context.moveTo( GRAPH_LEFT, ( GRAPH_BOTTOM ) / 4 );
-        context.lineTo( GRAPH_RIGHT, ( GRAPH_BOTTOM ) / 4 );
-        context.globalAlpha = 0.3
-        context.strokeStyle = "#BBB";
-        context.stroke();
-
-        context.beginPath();
-        context.moveTo( GRAPH_LEFT, ( GRAPH_BOTTOM ) / 1.3333 );
-        context.lineTo( GRAPH_RIGHT, ( GRAPH_BOTTOM ) / 1.3333 );
-        context.globalAlpha = 0.3
-        context.strokeStyle = "#BBB";
-        context.stroke();
-
-        // context.fillText("X", (cWidth + cleftMargin) /2, cHeight);
-        // context.fillText("Length (min)", -(cHeight +cTopMargin) / 2, CleftMargin)
-
-        maand = date.substring(5,7)
-        year = date.substring(0,4)
-        yearM = 20171101
-
-        var canvas = document.getElementById( "Canvas" );
-        var ctx = canvas.getContext( "2d" );
-
-        for (var x = 0; x < 10; x++)
-        {
-          console.log(x);
-          ctx.moveTo(0 + 48 * x, 0)
-          ctx.lineTo(0 + 48 * x, 480)
-          ctx.fillText(maand + "-" + year, GRAPH_RIGHT / datums.length + GRAPH_LEFT, GRAPH_BOTTOM)
-        }
-        ctx.stroke();
-
-        // context.beginPath();
-    //     for (var i = 0; i < datums.length; i++){
-    //       console.log(yearM)
-    //       if (yearM > 20171200 && yearM <2018000){
-    //         yearM = yearM +8900
-    //       }
-    //       if (parseInt(datums[i]) === yearM) {
-    //         console.log(124124);
-    //         yearM = yearM + 100
-    //         context.moveTo(GRAPH_RIGHT + yearM, GRAPH_BOTTOM)
-    //         context.lineTo(GRAPH_RIGHT + yearM, GRAPH_TOP)
-    //         //context.front
-    //         context.fillText(maand + "-" + year, GRAPH_RIGHT / datums.length + GRAPH_LEFT, GRAPH_BOTTOM)
-    //
-    //     }
-    // }
-
-
-
-  }
+        graph(data, datums, listMaand, listYear)
+    }
 }
 
 txtFile.open("GET", fileName);
--- Homework/Week_3/KNMI.py
+++ Homework/Week_3/KNMI.py
@@ -1,11 +1,12 @@
+# Liora Rosenberg
+# Student number: 11036435
+
 import pandas
 import csv
 import json
 
 INPUT_CSV = "KNMI.csv"
-#"KNMI_min+max_temp_leeuwarden.csv"
 reader = pandas.read_csv(INPUT_CSV)
-print(reader)
 
 reader = reader.set_index('Date')
-reader = reader.to_json('x.json', orient="index")
+reader = reader.to_json('KNMI.json', orient="index")
